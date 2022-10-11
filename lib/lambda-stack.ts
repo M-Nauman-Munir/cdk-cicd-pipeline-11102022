ecde49c751ec5c6366a66fd2eb3ada2c855f1beb
M-Nauman-Munir <naumanmunir.akbl@gmail.com>
2022-10-11 18:18:45 +0500

    update

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Function, Code, Runtime } from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
 
 export class MyLambdaStack extends cdk.Stack {
   constructor(
@@ -12,11 +13,11 @@ export class MyLambdaStack extends cdk.Stack {
     super(scope, id, props);
 
     new Function(this, "LambdaFunction", {
-      code: Code.fromAsset("lambda"),
+      code: Code.fromAsset(path.join(__dirname, "lambda")),
       handler: "index.handler",
       runtime: Runtime.NODEJS_16_X,
       environment: {
-        stageName,
+        stageName: stageName,
       },
     });
   }
