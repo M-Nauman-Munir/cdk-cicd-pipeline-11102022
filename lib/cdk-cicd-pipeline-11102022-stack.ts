a045ea3996e042b895099a48f44d89297bb004d3
M-Nauman-Munir <naumanmunir.akbl@gmail.com>
2022-10-11 16:39:58 +0500

    Initial commit

---
+++ lib/cdk-cicd-pipeline-11102022-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkCicdPipeline11102022Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkCicdPipeline11102022Queue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/cdk-cicd-pipeline-11102022.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCicdPipeline11102022 from '../lib/cdk-cicd-pipeline-11102022-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-cicd-pipeline-11102022-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCicdPipeline11102022.CdkCicdPipeline11102022Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
