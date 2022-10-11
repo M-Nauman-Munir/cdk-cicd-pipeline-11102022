be186c46f03261391d9d335b34eb12c9cc840eff
M-Nauman-Munir <naumanmunir.akbl@gmail.com>
2022-10-11 18:07:47 +0500

    cicd pipeline

--- lib/cdk-cicd-pipeline-11102022-stack.ts
+++ lib/cdk-cicd-pipeline-11102022-stack.ts
@@ -1,17 +1,37 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as awsCodePipeline from 'aws-cdk-lib/pipelines';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as awsCodePipeline from "aws-cdk-lib/pipelines";
+import { MyPipelineAppStage } from "./stage";
 
 export class CdkCicdPipeline11102022Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new awsCodePipeline.CodePipeline(this, 'AWS-Code-Pipeline', {
-      pipelineName: 'test-code-pipeline',
-      synth: new awsCodePipeline.ShellStep('Synth', {
-        input: awsCodePipeline.CodePipelineSource.gitHub('M-Nauman-Munir/cdk-cicd-pipeline-11102022', 'master'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      })
-    })
+    const pipeline = new awsCodePipeline.CodePipeline(
+      this,
+      "AWS-Code-Pipeline",
+      {
+        pipelineName: "test-code-pipeline",
+        synth: new awsCodePipeline.ShellStep("Synth", {
+          input: awsCodePipeline.CodePipelineSource.gitHub(
+            "M-Nauman-Munir/cdk-cicd-pipeline-11102022",
+            "master",
+          ),
+          commands: ["npm ci", "npm run build", "npx cdk synth"],
+        }),
+      },
+    );
+
+    const testingStage = pipeline.addStage(
+      new MyPipelineAppStage(this, "test"),
+    );
+
+    testingStage.addPost(
+      new awsCodePipeline.ManualApprovalStep(
+        "Manual approval is needed before production",
+      ),
+    );
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod"));
   }
 }
